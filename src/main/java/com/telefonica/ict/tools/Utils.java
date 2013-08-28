e22a5bfc70dc4298cab67110accfd28f08f2932e
Ruben <rubenmodrono@gmail.com>
2013-08-28 07:49:49 +0200

    Nuevas clases de utilidad

---
+++ src/main/java/com/telefonica/ict/tools/Utils.java
@@ -0,0 +1,36 @@
+package com.telefonica.ict.tools;
+
+
+public class Utils {
+
+	/**
+	 * Clase que recibe el nombre del archivo xml para
+	 * @param archiveName
+	 * @return
+	 */
+	public static String decodeICTName(String archiveName){
+		
+		String name = "";
+		
+		String[] splittedString =  archiveName.split("_");
+		
+		for (String s:splittedString){
+			if (s.startsWith("COP")){
+				name = s;
+				break;
+			}
+		}
+
+		return name.replace(".xml", ""); 
+	}
+
+	public static String codeCoordinate(int grades, int minutes, int seconds, int decimals, String direction){
+			
+		Double result = (new Double(grades)+ (new Double (minutes))/60 + new Double (seconds+"."+decimals)/3600);
+		
+		result = direction.equals("S") || direction.equals("W") || direction.equals("O")?result*-1:result;
+		
+		return result.toString();
+	}
+	
+}
